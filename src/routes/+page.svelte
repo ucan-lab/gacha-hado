d8df604c5af8d7392b0c95c9c4b0d65e84443c19
ucan-lab <35098175+ucan-lab@users.noreply.github.com>
2025-01-25 22:08:07 +0900

    翻訳漏れ修正

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -108,7 +108,7 @@
         on:click={toggleModal}
         class="mt-4 rounded bg-red-500 px-6 py-2 text-white hover:bg-red-600"
       >
-        Close
+        {m.close()}
       </button>
     </div>
   </div>
