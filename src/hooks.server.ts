cdca48baa58a4bce6d0c8685656afae6c6d883da
ucan-lab <35098175+ucan-lab@users.noreply.github.com>
2025-03-03 05:34:07 +0900

    ERR_CONTENT_LENGTH_MISMATCH エラー対策

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,15 +6,23 @@ const handleParaglide: Handle = i18n.handle();
 export const handle: Handle = async ({ event, resolve }) => {
   const response = await handleParaglide({ event, resolve });
 
+  // Flash of Unstyled Content 対策でクッキーからテーマを取得
   let theme = event.cookies.get('theme') || 'light';
   if (theme === 'auto') {
     theme = 'light';
   }
 
-  const text = await response.text();
+  const responseText = await response.text();
+  const modifiedResponseText = responseText.replace('%THEME%', theme);
 
-  return new Response(text.replace('%THEME%', theme), {
+  // Content-Length を再計算しないと ERR_CONTENT_LENGTH_MISMATCH エラーが発生するため
+  const contentLength = new TextEncoder().encode(modifiedResponseText).length;
+  const headers = new Headers(response.headers);
+  headers.delete('Content-Length');
+  headers.set('Content-Length', contentLength.toString());
+
+  return new Response(modifiedResponseText, {
     status: response.status,
-    headers: response.headers
+    headers
   });
 };
